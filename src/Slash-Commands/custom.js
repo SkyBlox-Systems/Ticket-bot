dfd5222882897deb0ce6e7e7f60fceb5532b964d
SkyBlox <xxhackerminecraftpe2@gmail.com>
2024-01-30 08:52:14 +0000

    v6.2 Dev #2

--- src/Slash-Commands/custom.js
+++ src/Slash-Commands/custom.js
@@ -35,20 +35,25 @@ module.exports.data = new SlashCommandBuilder()
                     .setRequired(true)))
     .addSubcommand(subcommand =>
         subcommand
-        .setName('status')
-        .setDescription('Check the status of your bot'))
-    .addSubcommand(subcommand => 
-        subcommand
-        .setName('stop')
-        .setDescription('Stop your custom bot'))
+            .setName('status')
+            .setDescription('Check the status of your bot'))
     .addSubcommand(subcommand =>
         subcommand
-        .setName('restart')
-        .setDescription('Restart your custom bot'))
+            .setName('stop')
+            .setDescription('Stop your custom bot'))
     .addSubcommand(subcommand =>
         subcommand
-        .setName('delete')
-        .setDescription('Delete your custom bot'))
+            .setName('restart')
+            .setDescription('Restart your custom bot'))
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('delete')
+            .setDescription('Delete your custom bot'))
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('start')
+            .setDescription('Start your custom bot'))
+
 module.exports.run = (client, interaction) => {
     const clientidbot = interaction.options.getString('clientid')
     const clientsecretbot = interaction.options.getString('secret')
@@ -64,58 +69,69 @@ module.exports.run = (client, interaction) => {
         .setColor('#f9f9fa')
 
 
-    if (interaction.options.getSubcommand() === 'create') {
+    if (interaction.options.getSubcommand() === 'create') { // Create the container for the customer
         if (interaction.user.id != interaction.guild.ownerId)
             return interaction.reply({ embeds: [ServerOwner] });
         MainDatabase.findOne({ ServerID: interaction.guild.id }, async (err, data) => {
             if (err) throw err;
             if (data) {
                 if (data.PaidGuild === 'Yes') {
+                    if (data.CustomBots === '1') {
+                        interaction.reply('You already have a custom bot. Please delete that one first.')
+                    } else {
 
-
-                    let data = JSON.stringify({
-                        "Image": "skybloxsystemsltd/ticket-bot:Latest",
-                        "Env": [
-                            `CLIENTID=${clientidbot}`,
-                            `CLIENTSEC=${clientsecretbot}`,
-                            `TOKEN=${tokenbot}`,
-                            `BotOwnerID=${owneridbot}`
-                        ]
-                    });
-
-                    let config = {
-                        method: 'post',
-                        maxBodyLength: Infinity,
-                        url: `https://portainer.skybloxsystems.com/api/endpoints/2/docker/containers/create?name=${interaction.guild.id}&X-API-Key=ptr_5RIzgQaLyxFlZ8mjidhGUdmoyMSzQGFSvrR4GbtHj8A=`,
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        data: data
-                    };
-
-                    let config2 = {
-                        method: 'post',
-                        maxBodyLength: Infinity,
-                        url: `https://portainer.skybloxsystems.com/api/endpoints/2/docker/containers/${interaction.guild.id}/start?X-API-Key=ptr_5RIzgQaLyxFlZ8mjidhGUdmoyMSzQGFSvrR4GbtHj8A=`,
-                        headers: {}
-                    };
-
-                    axios.request(config)
-                        .then((response) => {
-                            interaction.reply('Bot has been created and will start shortly!')
-                            setTimeout(() => {
-                                axios.request(config2)
-                                    .then((response) => {
-                                        console.log(JSON.stringify(response.data));
-                                    })
-                                    .catch((error) => {
-                                        console.log(error);
-                                    });
-                            }, 3000);
-                        })
-                        .catch((error) => {
-                            console.log(error);
+                        let data = JSON.stringify({
+                            "Image": "skybloxsystemsltd/ticket-bot:Latest",
+                            "Env": [
+                                `CLIENTID=${clientidbot}`,
+                                `CLIENTSEC=${clientsecretbot}`,
+                                `TOKEN=${tokenbot}`,
+                                `BotOwnerID=${owneridbot}`
+                            ]
                         });
+
+                        let config = {
+                            method: 'post',
+                            maxBodyLength: Infinity,
+                            url: `https://portainer.skybloxsystems.com/api/endpoints/2/docker/containers/create?name=${interaction.guild.id}&X-API-Key=ptr_5RIzgQaLyxFlZ8mjidhGUdmoyMSzQGFSvrR4GbtHj8A=`,
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            data: data
+                        };
+
+                        let config2 = {
+                            method: 'post',
+                            maxBodyLength: Infinity,
+                            url: `https://portainer.skybloxsystems.com/api/endpoints/2/docker/containers/${interaction.guild.id}/start?X-API-Key=ptr_5RIzgQaLyxFlZ8mjidhGUdmoyMSzQGFSvrR4GbtHj8A=`,
+                            headers: {}
+                        };
+
+                        axios.request(config)
+                            .then((response) => {
+                                interaction.reply('Bot has been created and will start shortly!')
+                                setTimeout(() => {
+                                    axios.request(config2)
+                                        .then((response) => {
+                                            interaction.channel.send('The custom bot has been created, and the bot has started. You can now may remove this bot')
+                                            MainDatabase.findOneAndUpdate({ ServerID: interaction.guild.id }, { CustomBots: '1' }, (err2, data2) => {
+                                                if (err2) throw err;
+                                                if (data2) {
+                                                    data2.save()
+                                                }
+                                            })
+                                        })
+                                        .catch((error) => {
+                                            console.log(error);
+                                            interaction.reply('An error has occurred!')
+                                        });
+                                }, 3000);
+                            })
+                            .catch((error) => {
+                                console.log(error);
+                            });
+                    }
+
                 } else {
                     if (data.PaidGuild === 'No') {
                         interaction.reply('This is not a premium server. This command can not be used.')
@@ -123,6 +139,109 @@ module.exports.run = (client, interaction) => {
                 }
             }
         })
+    }
+
+    if (interaction.options.getSubcommand() === 'status') {  // Status of the container 
+        if (interaction.user.id != interaction.guild.ownerId)
+            return interaction.reply({ embeds: [ServerOwner] });
+
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: `https://portainer.skybloxsystems.com/api/endpoints/2/docker/containers/${interaction.guild.id}/json?X-API-Key=ptr_5RIzgQaLyxFlZ8mjidhGUdmoyMSzQGFSvrR4GbtHj8A=`,
+            headers: {}
+        };
+
+        axios.request(config)
+            .then((response) => {
+                const containerdata = new EmbedBuilder()
+                    .setTitle('Status')
+                    .setDescription('Get the status of your container')
+                    .addFields(
+                        { name: 'Name', value: interaction.guild.id, inline: true },
+                        { name: 'Status', value: response.data.State.Status, inline: true },
+                        { name: 'Start time', value: response.data.State.StartedAt, inline: true },
+                        { name: 'Created time', value: response.data.Created, inline: true },
+                        { name: 'Container ID', value: response.data.Config.Hostname, inline: true },
+                    )
+                    .setFooter({ text: 'Container ID is only used if there is a issue with the bot. Send that to customer service if needed.' })
+                interaction.reply({ embeds: [containerdata] })
+            })
+            .catch((error) => {
+                console.log(error);
+                interaction.reply('An error has occurred!')
+            });
+    }
+
+    if (interaction.options.getSubcommand() === 'stop') { // Stop the container
+        if (interaction.user.id != interaction.guild.ownerId)
+            return interaction.reply({ embeds: [ServerOwner] });
+
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: `https://portainer.skybloxsystems.com/api/endpoints/2/docker/containers/${interaction.guild.id}/stop?X-API-Key=ptr_5RIzgQaLyxFlZ8mjidhGUdmoyMSzQGFSvrR4GbtHj8A=`,
+            headers: {}
+        };
+
+        axios.request(config)
+            .then((response) => {
+                interaction.reply('You bot has been stopped successfully.')
+            })
+            .catch((error) => {
+                console.log(error);
+                interaction.reply('An error has occurred!')
+            });
+    }
+
+    if (interaction.options.getSubcommand() === 'restart') { // Restart the container
+        if (interaction.user.id != interaction.guild.ownerId)
+            return interaction.reply({ embeds: [ServerOwner] });
+
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: `https://portainer.skybloxsystems.com/api/endpoints/2/docker/containers/${interaction.guild.id}/restart?X-API-Key=ptr_5RIzgQaLyxFlZ8mjidhGUdmoyMSzQGFSvrR4GbtHj8A=`,
+            headers: {}
+        };
+
+        axios.request(config)
+            .then((response) => {
+                interaction.reply('You bot has been restarted successfully.')
+            })
+            .catch((error) => {
+                console.log(error);
+                interaction.reply('An error has occurred!')
+            });
+
+    }
+
+    if (interaction.options.getSubcommand() === 'delete') { // Delete the container -- Needs to be done
+        if (interaction.user.id != interaction.guild.ownerId)
+            return interaction.reply({ embeds: [ServerOwner] });
+    }
+
+    if (interaction.options.getSubcommand() === 'start') { // start the container
+        if (interaction.user.id != interaction.guild.ownerId)
+            return interaction.reply({ embeds: [ServerOwner] });
+
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: `https://portainer.skybloxsystems.com/api/endpoints/2/docker/containers/${interaction.guild.id}/start?X-API-Key=ptr_5RIzgQaLyxFlZ8mjidhGUdmoyMSzQGFSvrR4GbtHj8A=`,
+            headers: {}
+        };
+
+        axios.request(config)
+            .then((response) => {
+                interaction.reply('You bot has been started successfully.')
+            })
+            .catch((error) => {
+                console.log(error);
+                interaction.reply('An error has occurred!')
+
+            });
+
     }
 
 }
